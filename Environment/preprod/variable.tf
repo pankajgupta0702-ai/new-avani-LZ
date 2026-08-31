f759efcda63f98c4175de46dc5b7c7f4360205a3
PANKAJ GUPTA <pankaj.gupta0702@gmail.com>
2026-09-01 03:15:57 +0530

    pipeline added4

--- Environment/preprod/variable.tf
+++ Environment/preprod/variable.tf
@@ -1,7 +1,15 @@
 variable "resource_group" {
-  type = map(object({}))
+  type = map(object({
+    name     = string
+    location = string
+  }))
 }
 
 variable "virtual_network" {
-  type = map(object({}))
+  type = map(object({
+    name                = string
+    resource_group_name = string
+    location            = string
+    address_space       = list(string)
+  }))
 }
